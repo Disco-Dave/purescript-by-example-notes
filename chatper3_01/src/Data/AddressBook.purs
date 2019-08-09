715113584c81dbaf22f90d9c0f4acdbc5dd360c5
David Burkett <db.discodave@gmail.com>
2019-08-09 18:10:31 -0400

    FInished reading chapter 3

--- chatper3_01/src/Data/AddressBook.purs
+++ chatper3_01/src/Data/AddressBook.purs
@@ -3,7 +3,7 @@ module Data.AddressBook where
 import Prelude
 
 import Control.Plus (empty)
-import Data.List (List(..), filter, head)
+import Data.List (List, filter, head, (:))
 import Data.Maybe (Maybe)
 
 type Entry =
@@ -31,3 +31,14 @@ showAddress address =
     <> address.state
 
 type AddressBook = List Entry
+
+emptyBook :: AddressBook
+emptyBook = empty
+
+insertEntry :: Entry -> AddressBook -> AddressBook
+insertEntry = (:)
+
+findEntry :: String -> String -> AddressBook -> Maybe Entry
+findEntry firstName lastName =
+    let isEntry entry = firstName == entry.firstName && lastName == entry.lastName
+    in  filter isEntry >>> head
