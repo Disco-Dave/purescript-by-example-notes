f1cc37e38c34a47e14752ca2d905f8647144eb09
David Burkett <db.discodave@gmail.com>
2019-08-10 17:06:00 -0400

    Finished chapter 3 exercsies

--- chatper3_01/src/Data/AddressBook.purs
+++ chatper3_01/src/Data/AddressBook.purs
@@ -3,8 +3,8 @@ module Data.AddressBook where
 import Prelude
 
 import Control.Plus (empty)
-import Data.List (List, filter, head, (:))
-import Data.Maybe (Maybe)
+import Data.List (List, filter, head, (:), nubBy)
+import Data.Maybe (Maybe, isNothing)
 
 type Entry =
     { firstName :: String
@@ -18,14 +18,14 @@ showEntry entry =
     <> entry.firstName <> ": " 
     <> showAddress entry.address
 
-newtype Address = Address
+type Address =
     { street :: String
     , city :: String
     , state :: String
     }
 
 showAddress :: Address -> String
-showAddress (Address address) =
+showAddress address =
     address.street <> ", " 
     <> address.city <> ", " 
     <> address.state
@@ -38,11 +38,24 @@ emptyBook = empty
 insertEntry :: Entry -> AddressBook -> AddressBook
 insertEntry = (:)
 
-data AddressQuery
+data EntryQuery
     = ByName {firstName :: String, lastName :: String}
     | ByAddress {street :: String, city :: String, state :: String}
 
-findEntry :: String -> String -> AddressBook -> Maybe Entry
-findEntry firstName lastName =
-    let isEntry entry = firstName == entry.firstName && lastName == entry.lastName
-    in  filter isEntry >>> head
+findEntry :: EntryQuery -> AddressBook -> Maybe Entry
+findEntry entryQuery = head <<< filter (isEntry entryQuery)
+    where isEntry (ByName nameQuery) entry = 
+            nameQuery.firstName == entry.firstName && nameQuery.lastName == entry.lastName
+          isEntry (ByAddress addressQuery) entry =
+              addressQuery.street == entry.address.street && 
+              addressQuery.city == entry.address.city && 
+              addressQuery.state == entry.address.state
+
+containsEntry :: String -> String -> AddressBook -> Boolean
+containsEntry firstName lastName = isNothing <<< findEntry query
+    where query = ByName {firstName : firstName, lastName : lastName}
+
+removeDuplicates :: AddressBook -> AddressBook
+removeDuplicates = nubBy isDup
+    where isDup entry1 entry2 = entry1.firstName == entry2.firstName && 
+                                entry1.lastName == entry2.lastName
