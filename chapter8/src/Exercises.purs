ee695e4f35007a91deed94795809cef1df2a87ea
David Burkett <db.discodave@gmail.com>
2019-08-24 16:04:47 -0400

    Finished chapter 8

--- chapter8/src/Exercises.purs
+++ chapter8/src/Exercises.purs
@@ -1,10 +1,27 @@
 module Exercises where
 
 import Prelude
-import Data.Maybe (Maybe)
 import Data.Array as Array
+import Data.Maybe (Maybe)
+import Data.List (List(..))
 
 third :: forall a. Array a -> Maybe a
 third arr = do
   thirdItem <- Array.tail arr >>= Array.tail >>= Array.head
   pure thirdItem
+
+sums :: Array Int -> Array Int
+sums coins = Array.sort coinSums
+  where
+  uniqueCoins = Array.nub coins
+
+  coinSums = Array.foldM (\a e -> [ a, e + a ]) 0 uniqueCoins
+
+filterM :: forall m a. Monad m => (a -> m Boolean) -> List a -> m (List a)
+filterM predicate Nil = pure Nil
+filterM predicate (Cons h t) = do
+  shouldKeep <- predicate h
+  if shouldKeep then
+    Cons h <$> filterM predicate t
+  else
+    filterM predicate t
